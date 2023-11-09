560a0d09f8afb1cc81062e70e9e42f2e94fa8ae0
ProfessorMarcio <125913715+ProfessorMarcio@users.noreply.github.com>
2023-11-09 15:14:01 -0300

    Java

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-alert(Olá mundo);
+//criando a função tocaSomAplausos e buscando a id da tecla para TransformStreamDefaultController.
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+//referência constante listaDeTeclas que busca todos botões com a .tecla
+const listaDeTeclas = document.querySelectorAll(".tecla");
+//laço de repetição While trocado pelo "for" tecla, efeito, idAudio e tocaSom 
+for(let contador = 0;contador < listaDeTeclas.length);contador = contador++){
+    const tecla = listaDeTeclas[contador];
+    const efeito = tecla.classList[1]; // busca do item 1 dos botões
+    const idAudio = '#som_${efeito}'; // uso do template string
+    tecla.onclick = function(){ //função anônima auxiliar ao tocaSom
+        tocaSom(idAudio);
+    }
+    //console.log(contador);
+}
